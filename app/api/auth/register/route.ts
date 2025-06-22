8b936bb290f7f081882045b87348011d1edf3655
jamanul <jk.skb.25@gmail.com>
2025-06-22 19:01:29 +0600

    user crated

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,3 +1,37 @@
 import { connectToDB } from "@/lip/db";
 import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request:NextRequest){
+    try {
+       const {email,password}=  await request.json();
+
+       if(!email || !password){
+        return NextResponse.json({
+            error: "email not found"
+            
+        },{
+            status:400
+        })
+       }
+       await connectToDB();
+       const existingUser = await User.findOne({email})
+       if(existingUser){
+         return NextResponse.json({
+            error: "User already Registered"
+            
+        },{
+            status:400
+        })
+       }
+       await User.create({
+        email,password
+       })
+       return NextResponse.json(
+        {message:"User has been created successfully"},
+        {status:200}
+    )
+    } catch (error) {
+        
+    }
+}
